d95b7a2289e0af726f7e2b01c3b0f307f2849e7b
KamaProgman <latipovkamran1@gmail.com>
2024-01-28 18:10:08 +0500

    change of the folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import Layout from "./layouts/layout";
-import Home from "./pages/home/home";
-import Info from "./pages/info/info";
-import About from "./pages/about/about";
-import Problems from "./pages/problems/problem";
-import Service from "./pages/service/service";
-import BMI from "./pages/bmi/bmi";
-import Results from "./pages/results/results";
+import Home from "./sections/home/home";
+import Info from "./sections/info/info";
+import About from "./sections/about/about";
+import Problems from "./sections/problems/problem";
+import Service from "./sections/service/service";
+import BMI from "./sections/bmi/bmi";
+import Results from "./sections/results/results";
 
 function App() {
 	return (
