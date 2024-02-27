117b8ff4c2e81050d8cb807fbc586e878ffa40d1
KamaProgman <latipovkamran1@gmail.com>
2024-02-28 00:18:39 +0500

    adding varcel-analytics

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../components/header/header";
 import Footer from "../components/footer/footer";
+import { Analytics } from "@vercel/analytics/react"
 
 interface props {
 	children: React.ReactNode;
@@ -12,6 +13,8 @@ const Layout: React.FC<props> = ({ children }) => {
 			<Header />
 			<main>{children}</main>
 			<Footer />
+
+			<Analytics />
 		</>
 	);
 };
